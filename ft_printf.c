cc314a59c86c1f7bf7177b40e01607521be54ee9
KonstantinosLamprakis <28678729+KonstantinosLamprakis@users.noreply.github.com>
2024-03-13 13:46:55 +0100

    Add some notes

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: klamprak <klamprak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 07:45:41 by klamprak          #+#    #+#             */
-/*   Updated: 2024/03/13 12:20:10 by klamprak         ###   ########.fr       */
+/*   Updated: 2024/03/13 13:46:33 by klamprak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,26 +42,33 @@ int	ft_printf(const char *format, ...)
 	return (printed_num);
 }
 
-// #include <limits.h>
-// int	main(void)
-// {
-// 	int b, c;
-// 	char *test = "this is me";
+#include <limits.h>
+int	main(void)
+{
+	int b, c;
+	char *test = "this is me";
 
-// 	b = 0;
-// 	c = 0;
-// 	// printf("%15.*s", 5, "this is a test");
-// 	// printf("% d", 5);
-// 	// printf("%3d", 1243);
+	b = 0;
+	c = 0;
+	// printf("%15.*s", 5, "this is a test");
+	// printf("% d", 5);
+	// printf("%3d", 1243);
 
-// 	c = ft_printf(" %-1c %-2c %-3c\n", '0', 0, '1');
-// 	b = printf(" %-1c %-2c %-3c\n", '0', 0, '1');
+	c = ft_printf("%010.2i\n", -8473);
+	b = printf("%010.2i\n", -8473);
 
-// 	printf("-%d %d-\n", c , b);
-// 	// system("leaks a.out");
-// }
+	printf("-%d %d-\n", c , b);
+	// system("leaks a.out");
+}
 
 // b = printf("%08.4d\n", -14);
+
+// c = ft_printf(">------------<%58.24d>------------<\n", 0);
+// b = printf(">------------<%58.24d>------------<\n", 0);
+
+// c = ft_printf("%08.3i\n", -8473);
+// b = printf("%08.3i\n", -8473);
+
 // . adds zeros in front of number like d i u not spase
 // . put after the zeros the minus while field_width before
 // . ensure the number to have the exact size without the sign,
@@ -69,3 +76,7 @@ int	ft_printf(const char *format, ...)
 // field width ensures the minus is before space
 	// or after zero if its a number
 // ("%.s", NULL) -> should print only "" not (null)
+	// if negative number, zero go between minus and number until reach width..if . exists overatis 0 with " "
+// if is ' ' filled before '-' on negative while if is '0' filled after the minus
+// pressision always filled with zeros no matter what
+
